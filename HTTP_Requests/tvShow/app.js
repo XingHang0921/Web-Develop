39d6ef5ae1ba6d25d9647427089c5a50fe9977e0
Nevin <nevinzheng0921@gmail.com>
2025-09-07 22:31:34 -0400

    simple loading images

---
+++ HTTP_Requests/tvShow/app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('#searchForm')
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault()
+    const searchTerm = form.children.query.value
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    addImages(res.data)
+
+    
+})
+
+const addImages = (shows) => {
+    for(let result of shows){
+        if(result.show.image){
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }
+} 
