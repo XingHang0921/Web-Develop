83ad6cfa42e8573850a310e69ad7baaaed489280
xingh <nevinzheng0921@gmail.com>
2025-09-12 16:37:39 -0400

    file manangement

--- node/boilterPlate.js
+++ node/boilterPlate.js
@@ -1,4 +1,4 @@
-import { mkdir, mkdirSync } from 'node:fs';
+import { mkdir, mkdirSync, writeFileSync } from 'node:fs';
 
 // // Create ./tmp/a/apple, regardless of whether ./tmp and ./tmp/a exist.
 // mkdir('./tmp/a/apple', { recursive: true }, (err) => {
@@ -6,6 +6,15 @@ import { mkdir, mkdirSync } from 'node:fs';
 //     if (err) throw err;
 // });
 // console.log('im after callback')
+const folderName = process.argv[2] || 'Project'
 
-mkdirSync('cats');
-console.log('i come after make dir')
+
+try{
+    mkdirSync(folderName);
+    writeFileSync(`${folderName}/index.html`);
+    writeFileSync(`${folderName}/app.css`);
+    writeFileSync(`${folderName}/app.js`);
+}
+catch(e){
+    console.log("soemthing wrong " , e)
+}
