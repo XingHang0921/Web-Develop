9bb416e93b485c0fa6f8c830bbb9010db42d0b1a
Nevin <nevinzheng0921@gmail.com>
2025-09-29 22:17:13 -0400

    fix the issue the express.static requires full path using join __dirname

--- templatingDemo/index.js
+++ templatingDemo/index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path')
 const redditData = require('./data.json')
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('view engine', 'ejs'); //tell app to use ejs
 app.set('views', path.join(__dirname, 'views')) 
 //help to access the views and run the app outside of this dir
