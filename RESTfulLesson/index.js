117abcf520876f81465b049806c83cc480830ed8
xingh <nevinzheng0921@gmail.com>
2025-10-02 12:19:56 -0400

    post get, postman json body

---
+++ RESTfulLesson/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get('/')
+app.get('/tacos', (req, res) =>{
+    res.send('get /tacos response')
+})
+
+app.post('/tacos', (req,res)=>{
+    console.log(req.body)
+    res.send(`get post response`)
+})
+app.listen(3000, ()=>{
+    console.log('on port 3000')
+})
+
+
