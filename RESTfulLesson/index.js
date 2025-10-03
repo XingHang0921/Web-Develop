789c575dac5dcd06df61f490c239ec85e92831b7
Nevin <nevinzheng0921@gmail.com>
2025-10-03 13:37:51 -0400

    Override_Method_Delete-> full CRUD finished

--- RESTfulLesson/index.js
+++ RESTfulLesson/index.js
@@ -10,7 +10,7 @@ app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
 
-const comments = [
+let comments = [
     {
         id:uuid(),
         username: 'Todd',
@@ -61,6 +61,13 @@ app.patch('/comments/:id', (req, res)=>{
     const newCommentText = req.body.comment;
     const foundComment = comments.find(c => c.id === id)
     foundComment.comment = newCommentText;
+    res.redirect('/comments')
+})
+
+app.delete('/comments/:id', (req,res)=>{
+    const {id} = req.params;
+    const foundComment = comments.find(c => c.id === id)
+    comments = comments.filter(c => c.id !== id)
     res.redirect('/comments')
 })
 app.listen(3000, ()=>{
