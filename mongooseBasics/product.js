993fcc6b2c582728e959daa4d4c17fcdf2bff46d
Nevin <nevinzheng0921@gmail.com>
2025-10-04 14:03:06 -0400

    Schema Validations, default, nested

--- mongooseBasics/product.js
+++ mongooseBasics/product.js
@@ -10,17 +10,32 @@ mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxLength: 20,
     },
     price: {
-        type:Number
+        type:Number,
+        require:true,
+        min:0,
     },
+    categories: [],
+    qty:{
+        online:{
+            type:Number,
+            default:0
+        },
+        inStore:{
+            type:Number,
+            default:0
+        }
+    }
 
 })
 
 const Product = mongoose.model('Product', productSchema)
 
-const bike = new Product({name: 'Mountain Bike', price: 599})
+const bike = new Product({name: 'Mountain Bike', price:2123
+        , categories:['Cycleing', 'safety']})
 bike.save()
 .then(data =>{
     console.log('it work ' + data)
