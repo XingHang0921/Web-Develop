e2f05a523aa7c255cbfe151dde6d68eaf5677f8a
Nevin <nevinzheng0921@gmail.com>
2025-10-03 22:52:50 -0400

    insertMany Promise

--- mongooseBasics/index.js
+++ mongooseBasics/index.js
@@ -16,3 +16,40 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model('Movie', movieSchema);
 const amadeus = new Movie({title:'Amadeus', year:1986, score:9.2, rating:5})
+
+Movie.insertMany([
+    {
+        title: "The Matrix",
+        year: 1999,
+        score: 9.0,
+        rating: 5
+    },
+    {
+        title: "Inception",
+        year: 2010,
+        score: 8.8,
+        rating: 4
+    },
+    {
+        title: "Interstellar",
+        year: 2014,
+        score: 8.6,
+        rating: 5
+    },
+    {
+        title: "The Dark Knight",
+        year: 2008,
+        score: 9.1,
+        rating: 5
+    },
+    {
+        title: "Parasite",
+        year: 2019,
+        score: 8.6,
+        rating: 4
+    }
+])
+    .then(data =>{
+        console.log('it worked')
+        console.log(data);
+    })
