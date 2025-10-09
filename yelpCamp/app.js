7412eeeada66385e82fe703dc0ccd6da845ebbb2
xingh <nevinzheng0921@gmail.com>
2025-10-09 14:39:19 -0400

    added Creating Route

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
-
+app.use(express.urlencoded({extended:true}))
 
 app.get('/home', (req,res)=>{
     res.render('home')
@@ -24,6 +24,16 @@ app.get('/home', (req,res)=>{
 app.get('/campgrounds', async (req,res)=>{
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', {campgrounds})
+})
+
+app.get('/campgrounds/new', (req, res) =>{
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req,res)=>{
+    const campground = new Campground(req.body)
+    await campground.save()
+    res.redirect('/campgrounds')
 })
 
 app.get('/campgrounds/:id', async (req,res)=>{
