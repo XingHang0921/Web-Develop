cde6fb96917c38e74fe1b626607b0ebb2ab6e7dc
Nevin <nevinzheng0921@gmail.com>
2025-10-18 14:10:46 -0400

    catchAsync, added generic error catching

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const Campground = require('./models/campGround')
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
 mongoose.connect('mongodb://localhost:27017/yelpCamp')
 
 const db = mongoose.connection;
@@ -24,48 +25,44 @@ app.get('/home', (req,res)=>{
     res.render('home')
 })
 
-app.get('/campgrounds', async (req,res)=>{
+app.get('/campgrounds', catchAsync(async (req,res)=>{
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', {campgrounds})
-})
+}))
 
 app.get('/campgrounds/new', (req, res) =>{
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req,res)=>{
-    try{
+app.post('/campgrounds', catchAsync(async (req,res)=>{
         const campground = new Campground(req.body)
         await campground.save()
         res.redirect('campgrounds')
-    }catch(e){
-        next(e)
-    }
     
-})
+}))
 
-app.get('/campgrounds/:id', async (req,res)=>{
+app.get('/campgrounds/:id', catchAsync(async (req,res)=>{
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show',{campground})
-})
+}))
 
-app.get('/campgrounds/:id/edit', async(req, res)=>{
+app.get('/campgrounds/:id/edit', catchAsync(async(req, res)=>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit',{campground})
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}))
 
-app.delete('/campgrounds/:id', async(req, res)=>{
+app.delete('/campgrounds/:id', catchAsync(async(req, res)=>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
-})
+}))
 
 app.use((err, req, res, next) => {
     res.send('oh boy, something went wrong!')
