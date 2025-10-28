09a1f46c32897505583d5c253bb40c282588f59a
Nevin <nevinzheng0921@gmail.com>
2025-10-27 22:41:47 -0400

    added flash to success and error

--- yelpCamp/routes/campgrounds.js
+++ yelpCamp/routes/campgrounds.js
@@ -32,24 +32,34 @@ router.post('/', validateCampground, catchAsync(async (req,res)=>{
 
 router.get('/:id', catchAsync(async (req,res)=>{
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error', 'Cannot find that campground');
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show',{campground})
 }))
 
 router.get('/:id/edit', catchAsync(async(req, res)=>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
+    if (!campground) {
+      req.flash("error", "Cannot find that campground");
+      return res.redirect("/campgrounds");
+    }
     res.render('campgrounds/edit',{campground})
 }))
 
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
+    req.flash('success', 'Successfully updated the Campground')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.delete('/:id', catchAsync(async(req, res)=>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "Sucessfully delete a Campground");
     res.redirect('/campgrounds')
 }))
 
