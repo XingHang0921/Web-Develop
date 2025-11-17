d69b86709cba7a9027318868b411e0a5453fc0b9
Nevin <nevinzheng0921@gmail.com>
2025-11-17 12:17:01 -0500

    file input

--- yelpCamp/routes/campgrounds.js
+++ yelpCamp/routes/campgrounds.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
 const { isLoggedIn, validateCampground, isAuthor} = require('../middleware.js');
 const campground = require('../controllers/campground.js')
-
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router.route('/')
     .get(catchAsync(campground.index))
-    .post(validateCampground, catchAsync(campground.createCampground))
-
+    // .post(validateCampground, catchAsync(campground.createCampground))
+    .post(upload.array('image'),(req, res) =>{
+        console.log(req.body, req.files)
+    })
 router.get('/new',isLoggedIn, campground.newPage)
 
 router.route('/:id')
