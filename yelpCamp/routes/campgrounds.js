e4e6cf0643036d635529fc40bb09dfc903c110e6
xingh <nevinzheng0921@gmail.com>
2025-11-19 21:48:09 -0500

    changeing cloudinary route/ but keep consistence through device

--- yelpCamp/routes/campgrounds.js
+++ yelpCamp/routes/campgrounds.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
 const { isLoggedIn, validateCampground, isAuthor} = require('../middleware.js');
 const campground = require('../controllers/campground.js')
+const multer = require('multer')
+const storage = require('../cloudinary/index')
+const upload = multer({storage})
 
 
 router.route('/')
     .get(catchAsync(campground.index))
-    .post(validateCampground, catchAsync(campground.createCampground))
-
+    // .post(isLoggedIn, validateCampground, catchAsync(campground.createCampground))
+    .post(upload.array('image', (req,res)=>{
+        console.log(req.body,)
+    }))
 router.get('/new',isLoggedIn, campground.newPage)
 
 router.route('/:id')
