293554d2af6574fb4550957218893046db9cfdd2
Nevin <nevinzheng0921@gmail.com>
2025-12-13 22:58:09 -0500

    deleting image from cloudinary and mongoDB

--- yelpCamp/seeds/index.js
+++ yelpCamp/seeds/index.js
@@ -18,23 +18,23 @@ const seedDB = async()=>{
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-          author: "69038dbc5e11e46ef0db581d",
+          author: "69063c000c437e705db13737",
           title: `${sample(descriptors)} ${sample(places)}`,
           location: `${cities[random1000].city}, ${cities[random1000].state}`,
           images: [
-                {
-                  url: 'https://res.cloudinary.com/dd9x0c5zo/image/upload/v1764365361/YelpCamp/khtijklgbccacve9o5tg.png',
-                  filename: 'YelpCamp/khtijklgbccacve9o5tg',
-                },
-                {
-                  url: 'https://res.cloudinary.com/dd9x0c5zo/image/upload/v1764365363/YelpCamp/qiwyzh8kfo0bauiwhki5.png',
-                  filename: 'YelpCamp/qiwyzh8kfo0bauiwhki5',
-                },
-                {
-                  url: 'https://res.cloudinary.com/dd9x0c5zo/image/upload/v1764365365/YelpCamp/tb2chraveyfvse8az1bu.png',
-                  filename: 'YelpCamp/tb2chraveyfvse8az1bu',
-                }
-              ],
+            {
+              url: "https://res.cloudinary.com/dd9x0c5zo/image/upload/v1764365361/YelpCamp/khtijklgbccacve9o5tg.png",
+              filename: "YelpCamp/khtijklgbccacve9o5tg",
+            },
+            {
+              url: "https://res.cloudinary.com/dd9x0c5zo/image/upload/v1764365363/YelpCamp/qiwyzh8kfo0bauiwhki5.png",
+              filename: "YelpCamp/qiwyzh8kfo0bauiwhki5",
+            },
+            {
+              url: "https://res.cloudinary.com/dd9x0c5zo/image/upload/v1764365365/YelpCamp/tb2chraveyfvse8az1bu.png",
+              filename: "YelpCamp/tb2chraveyfvse8az1bu",
+            },
+          ],
           description:
             "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure aliquid officiis asperiores numquam esse architecto ipsum natus! Neque illum ea nihil debitis delectus corrupti, modi quas, voluptatem rerum in excepturi?",
           price,
