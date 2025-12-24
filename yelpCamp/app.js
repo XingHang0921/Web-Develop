ea7b03118da6d26f0fbeb699ba4acb84944c66e1
xingh <nevinzheng0921@gmail.com>
2025-12-24 14:11:20 -0500

    stash local

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -36,7 +36,7 @@ app.set('views', path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname,'public')))
-    app.use(ExpressMongoSanitize)
+    app.use(ExpressMongoSanitize) 
 
 const sessionConfig = {
   secret: "thisisthetopsecret!",
